7a1ee088665c490b086cdefcacc2d95fb4713ba3
tedschu <ted.schuster@gmail.com>
2024-05-28 12:14:04 -0500

    adding files

---
+++ index.js
@@ -0,0 +1,65 @@
+// import the pets array from data.js
+const pets = require("./data");
+
+// init express app
+const express = require("express");
+const app = express();
+
+const PORT = 8080;
+
+const baseQuery = `/api/`;
+
+// GET - / - returns homepage
+app.get("/", (req, res) => {
+  // serve up the public folder as static index.html file
+  res.sendFile(__dirname + "/public/index.html");
+});
+
+// hello world route
+app.get(baseQuery, (req, res) => {
+  res.send("Hello World!");
+});
+
+// get all pets from the database
+app.get(baseQuery + "v1/pets", (req, res) => {
+  // send the pets array as a response
+  res.send(pets);
+});
+
+// get pet by owner with query string
+app.get(baseQuery + "v1/pets/owner/", (req, res) => {
+  // get the owner from the request
+
+  const owner = req.query.owner.toLowerCase();
+  console.log(owner);
+
+  // find the pet in the pets array
+  const pet = pets.find((pet) => pet.owner.toLowerCase() === owner);
+  console.log(pet);
+
+  // send the pet as a response
+  res.send(pet);
+});
+
+// get pet by name
+app.get("/api/v1/pets/:name", (req, res) => {
+  // get the name from the request
+
+  const { name } = req.params;
+
+  // find the pet in the pets array
+  const pet = pets.find((pet) => pet.name.toLowerCase() === name);
+
+  // send the pet as a response
+  if (pet) {
+    res.send(pet);
+  } else {
+    res.send("Pet is not found");
+  }
+});
+
+app.listen(PORT, () => {
+  console.log("Server is listening on port " + PORT);
+});
+
+module.exports = app;
